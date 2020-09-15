5e000e8c52f3cc340c2a9fece9c921ae2f3e511b
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-09-15 00:04:43 -0700

    Build

---
+++ 2-Weekz/Assets/Script/NetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers = 10;
+    //singleton instance
+    public static NetworkManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();    
+    }
+
+    // attempt to create a room
+    public void CreateRoom(string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected Established");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void JoinRoom (string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
---
+++ 2-Weekz/Assets/Script/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class PlayerController : MonoBehaviourPun
+{
+    [Header("Stats")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Components")]
+    public Rigidbody rig;
+
+    public int id;
+    public Player photonPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        id = player.ActorNumber;
+        photonPlayer = player;
+
+        GameManager.instance.players[id - 1] = this;
+        if (!photonView.IsMine)
+        {
+            GetComponentInChildren<Camera>().gameObject.SetActive(false);
+            rig.isKinematic = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.Space))
+            TryJump();
+    }
+
+    void Move()
+    {
+        // get the input axis
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // calculate relative vector based on key input
+        Vector3 dir = (transform.forward * z + transform.right * x) * moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+    }
+
+    void TryJump()
+    {
+        // create ray pointing down
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 1.5f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
